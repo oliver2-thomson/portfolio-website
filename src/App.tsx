3000d9bc9ef8ec336168f74f66ae966b4381c4d4
Home PC <oliver2.thomson@live.uwe.ac.uk>
2024-07-07 22:52:15 +0100

    Added: content fills the remaining area

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {Box} from "@chakra-ui/react";
+import {Box, Flex} from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
@@ -12,26 +12,31 @@ function App() {
 
     return (
         <>
-
-            <Box
-                // positioning for the box around the navbar - change later
-                position="fixed"
-                left="5vw"
-                top="20vh"
-                height="60vh"
-                width="20vw"
-            >
-                <NavBar onSelectPage={(page) => setSelectedPage(page)}/>
-            </Box>
-            <Box backgroundColor="gold">
-                {selectedPage === 0 && <AboutMe/>}
-                {selectedPage === 1 && <Projects/>}
-                {selectedPage === 2 && <Skills/>}
-                {selectedPage === 3 && <Experience/>}
-                {selectedPage === 4 && <ContactMe/>}
-            </Box>
-
-
+            <Flex height="100vh">
+                <Box
+                    // positioning for the box around the navbar - change later
+                    position="fixed"
+                    left="5vw"
+                    top="20vh"
+                    height="60vh"
+                    width="20vw"
+                >
+                    <NavBar onSelectPage={(page) => setSelectedPage(page)}/>
+                </Box>
+                <Box
+                    //This doesnt seem like the best way to do this
+                    backgroundColor="gold"
+                    marginLeft="30vw"
+                    flex="1"
+                    overflow="auto"
+                >
+                    {selectedPage === 0 && <AboutMe/>}
+                    {selectedPage === 1 && <Projects/>}
+                    {selectedPage === 2 && <Skills/>}
+                    {selectedPage === 3 && <Experience/>}
+                    {selectedPage === 4 && <ContactMe/>}
+                </Box>
+            </Flex>
         </>
     );
 }
