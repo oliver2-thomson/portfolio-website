6bc71fd154a5eea30f8c7c419a688f6240024ed0
Home PC <oliver2.thomson@live.uwe.ac.uk>
2024-07-07 21:36:53 +0100

    Added: text on project component so scrolling can be tested

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,9 +1,14 @@
-import {Heading} from "@chakra-ui/react";
+import {Heading, Stack} from "@chakra-ui/react";
 
 const Projects = () => {
+    const longArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
     return (
         <>
-            <Heading>Projects</Heading>
+            <Stack align="center">
+                <Heading>Projects</Heading>
+                {longArray.map(() => <Heading>Content to fill page</Heading>)}
+            </Stack>
         </>
     );
 };
