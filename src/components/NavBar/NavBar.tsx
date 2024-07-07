bb530e5ad02426e4edaf9668e7da24161b76536a
Home PC <oliver2.thomson@live.uwe.ac.uk>
2024-07-07 18:29:20 +0100

    Added: basic NavBar buttons

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,15 @@
+import {Button, HStack} from "@chakra-ui/react";
+
 const NavBar = () => {
     return (
         <>
-            NavBar
+            <HStack justifyContent="space-around" marginY={3}>
+                <Button>About Me</Button>
+                <Button>Projects</Button>
+                <Button>Skills</Button>
+                <Button>Experience</Button>
+                <Button>Contact Me</Button>
+            </HStack>
         </>
     );
 };
