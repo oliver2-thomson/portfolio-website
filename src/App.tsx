21ccf55300bacee6bdc7d4277231dd795534cae8
Home PC <oliver2.thomson@live.uwe.ac.uk>
2024-07-07 18:25:50 +0100

    Added: NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import {Grid, GridItem} from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
 
-  return (
-    <>
-      <Grid templateAreas={`"nav" "main"`}>
-        <GridItem area='nav' bg='silver'>Nav</GridItem>
-        <GridItem area='main' bg='gold'>Main</GridItem>
-      </Grid>
-    </>
-  )
+    return (
+        <>
+            <Grid templateAreas={`"nav" "main"`}>
+                <GridItem area="nav" bg="silver">
+                    <NavBar/>
+                </GridItem>
+                <GridItem area="main" bg="gold">Main</GridItem>
+            </Grid>
+        </>
+    );
 }
 
-export default App
+export default App;
