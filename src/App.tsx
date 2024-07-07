2904cfd667988b36be8d785804193b271b0b23f3
Home PC <oliver2.thomson@live.uwe.ac.uk>
2024-07-07 18:21:57 +0100

    Added: Chakra UI grid for responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import {Grid, GridItem} from "@chakra-ui/react";
+
 function App() {
 
   return (
     <>
+      <Grid templateAreas={`"nav" "main"`}>
+        <GridItem area='nav' bg='silver'>Nav</GridItem>
+        <GridItem area='main' bg='gold'>Main</GridItem>
+      </Grid>
     </>
   )
 }
