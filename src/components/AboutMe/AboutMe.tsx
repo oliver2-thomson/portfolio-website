3bd90c4df898cc0cdec45a0a17547d18a50ef54d
Home PC <oliver2.thomson@live.uwe.ac.uk>
2024-07-07 18:54:26 +0100

    Added: text and default Image to AboutMe

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,7 +1,15 @@
+import {Heading, Image, Stack} from "@chakra-ui/react";
+import headshot from '../../assets/about-me-anonymous.svg';
+
 const AboutMe = () => {
     return (
         <>
-            About me
+            <Stack align="center">
+                <Heading>About me</Heading>
+                <Image src={headshot} boxSize={200} borderRadius={100}/>
+                <Heading>Oliver Thomson</Heading>
+                <Heading>Gameplay programmer</Heading>
+            </Stack>
         </>
     );
 };
