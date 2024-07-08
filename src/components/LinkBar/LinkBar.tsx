f90b7359d529bea03e61e6ec75cd88085c334c19
Home PC <oliver2.thomson@live.uwe.ac.uk>
2024-07-08 12:28:17 +0100

    Edit: made LinkBar icons larger

--- src/components/LinkBar/LinkBar.tsx
+++ src/components/LinkBar/LinkBar.tsx
@@ -1,9 +1,10 @@
 import {HStack, IconButton} from "@chakra-ui/react";
-import {FaGithub} from "react-icons/fa";
+import {FaGithub, FaLinkedin} from "react-icons/fa";
 
 const LinkBar = () => {
     const linkMap = [
-        {label: 'Github', icon: <FaGithub/>, link: 'https://github.com/oliver2-thomson'}
+        {label: 'Github', icon: <FaGithub/>, link: 'https://github.com/oliver2-thomson'},
+        {label: 'Linkedin', icon: <FaLinkedin/>, link: 'https://www.linkedin.com/in/oliver-thomson-272881309'}
     ];
 
     return (
@@ -18,6 +19,8 @@ const LinkBar = () => {
                             aria-label={link.label}
                             icon={link.icon}
                             variant="ghost"
+                            fontSize="30px"
+                            size="lg"
                         />
                     </a>
                 ))}
