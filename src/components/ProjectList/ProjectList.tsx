ffaf957afff39269016adc69c344614786a77382
Home PC <oliver2.thomson@live.uwe.ac.uk>
2024-07-09 23:20:35 +0100

    Added: loading state and error state for ProjectList component

--- src/components/ProjectList/ProjectList.tsx
+++ src/components/ProjectList/ProjectList.tsx
@@ -1,14 +1,16 @@
-import {Heading, Stack} from "@chakra-ui/react";
+import {Heading, Spinner, Stack, Text} from "@chakra-ui/react";
 import useProjects from "../../hooks/useProjects.ts";
 
 const ProjectList = () => {
     const {projects, isLoading, error} = useProjects();
 
+    if (error) return <Text color="red">{error}</Text>;
+
     return (
         <>
+            {isLoading && <Spinner/>}
             <Stack>
                 {projects.map(project => <Heading key={project.id}>{project.name}</Heading>)}
-
             </Stack>
         </>
     );
--- src/hooks/useProjects.ts
+++ src/hooks/useProjects.ts
@@ -1,43 +1,14 @@
-import {useEffect, useState} from "react";
-import apiClient from "../services/api-client.ts";
-import username from "../data/username.ts";
-import {CanceledError} from "axios";
+import useRepos from "./useRepos.ts";
 
-interface Project {
-    name: string;
+export interface Project {
     id: number;
+    name: string;
 }
 
 const useProjects = () => {
-    const [projects, setProjects] = useState<Project[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        const controller = new AbortController();
-
-        apiClient
-            //get the first 100 repos from the user - I am assuming there will never be >100 repos but for the sake of generality I should come back and make this iterable
-            .get(`https://api.github.com/users/${username}/repos`, {
-                signal: controller.signal,
-                params: {
-                    per_page: 100,
-                    page: 1
-                }
-            })
-            .then(res => {
-                    console.log(res.data);
-                    setProjects(res.data);
-                    setIsLoading(false); // this line must be repeated because .finally() is not supported for some reason
-                }
-            )
-            .catch(err => {
-                if (err instanceof CanceledError) return;
-                setError(err.message);
-                setIsLoading(false);
-            });
-    }, []);
-    return {projects, isLoading, error};
+    const repos = useRepos();
+    //filter for repos with the required tag here
+    return repos;
 };
 
 export default useProjects;
