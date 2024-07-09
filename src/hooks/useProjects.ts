a9ae3f53b9d755077e8fbbc328d4a5f501577511
Home PC <oliver2.thomson@live.uwe.ac.uk>
2024-07-09 23:43:17 +0100

    Added: can filter projects by topics on github

--- src/hooks/useProjects.ts
+++ src/hooks/useProjects.ts
@@ -3,12 +3,14 @@ import useRepos from "./useRepos.ts";
 export interface Project {
     id: number;
     name: string;
+    topics: string[];
 }
 
 const useProjects = () => {
-    const repos = useRepos();
+    const {projects, isLoading, error} = useRepos();
     //filter for repos with the required tag here
-    return repos;
+    const filteredProjects = projects.filter(project => project.topics.includes('portfolio-display'));
+    return {projects: filteredProjects, isLoading, error};
 };
 
 export default useProjects;
