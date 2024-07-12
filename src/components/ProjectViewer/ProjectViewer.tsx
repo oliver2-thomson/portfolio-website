57d6ee0bfd85eea1a123ad8fb2df222ba0e851f6
Laptop <oliver2.thomson@live.uwe.ac.uk>
2024-07-12 13:25:38 +0100

    Added: Props interface for ProjectViewer component

--- src/components/ProjectViewer/ProjectViewer.tsx
+++ src/components/ProjectViewer/ProjectViewer.tsx
@@ -1,4 +1,12 @@
-const ProjectViewer = () => {
+import {Project} from "../../hooks/useProjects.ts";
+
+interface Props {
+    project: Project;
+}
+
+const ProjectViewer = ({project}: Props) => {
+    
+
     return (
         <></>
     );
