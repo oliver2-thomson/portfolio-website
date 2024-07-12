d90ea55ec76214972050549b345da10a7a6d3b7c
Laptop <oliver2.thomson@live.uwe.ac.uk>
2024-07-12 22:09:22 +0100

    Edit: added inline styling to trick the iframe into scaling properly on smaller screens

--- src/components/ProjectViewer/ProjectViewer.tsx
+++ src/components/ProjectViewer/ProjectViewer.tsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {Flex} from "@chakra-ui/react";
+import {Button, Flex} from "@chakra-ui/react";
 
 interface Props {
     address: string;
@@ -14,10 +14,28 @@ const ProjectViewer = ({address}: Props) => {
 
     return (
         <>
-            {!buildUrl && <button onClick={handleLoadProject}>Load Project</button>}
+            {!buildUrl && <Button onClick={handleLoadProject}>Load Project</Button>}
             {buildUrl &&
-                <Flex width="100%" height="100%">
-                    <iframe src={buildUrl} width="100%" height="100%"></iframe>
+                <Flex
+                    width="100%"
+                    height="0"
+                    paddingTop="56.25%" /* 16:9 Aspect Ratio */
+                    position="relative"
+                    overflow="hidden"
+                >
+                    <iframe
+                        src={buildUrl}
+                        style={{
+                            width: '200%', /* Double the width to scale down */
+                            height: '200%', /* Double the height to scale down */
+                            transform: 'scale(0.5)', /* Scale down to fit */
+                            transformOrigin: '0 0',
+                            border: 'none',
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                        }}
+                    ></iframe>
                 </Flex>
             }
         </>
